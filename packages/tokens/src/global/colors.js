f02ffdb30add4c246c94ac3b5b5b01eb8b76f30d
alanoliv <alanbruno@gmail.com>
2020-03-13 11:19:34 -0300

    feat(tokens): adding new colors tokens and updating existent to new design definitions

--- packages/tokens/src/global/colors.js
+++ packages/tokens/src/global/colors.js
@@ -9,7 +9,22 @@
  * A color
  * @typedef Color
  *
- * @type {String | String[]}
+ * @type {Object}
+ * @property {number} i10 - color intensity 10
+ * @property {number} i20 - color intensity 20
+ * @property {number} i30 - color intensity 30
+ * @property {number} i40 - color intensity 40
+ * @property {number} i50 - color intensity 50
+ * @property {number} i60 - color intensity 60
+ * @property {number} i70 - color intensity 70
+ * @property {number} i80 - color intensity 80
+ */
+
+/**
+ * A feedbackcolor
+ * @typedef FeedbackColor
+ *
+ * @type {String[]}
  */
 
 /**
@@ -25,91 +40,254 @@ const madrid = [
   '#CB3530',
   '#A9221F',
 ];
+[
+  madrid.i10,
+  madrid.i20,
+  madrid.i30,
+  madrid.i40,
+  madrid.i50,
+  madrid.i60,
+  madrid.i70,
+  madrid.i80,
+] = madrid;
 
 /**
  * @type {Color}
  */
-const saoPaulo = ['#D8F5F5', '#58C2B4', '#2B9486'];
+const munich = [
+  '#FDECE0',
+  '#FADCC4',
+  '#FCBE94',
+  '#FFAC6F',
+  '#F69755',
+  '#E27B2D',
+  '#C76518',
+  '#AD5007',
+];
+[
+  munich.i10,
+  munich.i20,
+  munich.i30,
+  munich.i40,
+  munich.i50,
+  munich.i60,
+  munich.i70,
+  munich.i80,
+] = munich;
 
 /**
  * @type {Color}
  */
-const newYork = ['#EBECF9', '#3847C7', '#181E56'];
+const milan = [
+  '#FBFBBE',
+  '#FDEF8F',
+  '#FFE461',
+  '#F3D04A',
+  '#DFAF26',
+  '#D39E16',
+  '#BD8D10',
+  '#997108',
+];
+[
+  milan.i10,
+  milan.i20,
+  milan.i30,
+  milan.i40,
+  milan.i50,
+  milan.i60,
+  milan.i70,
+  milan.i80,
+] = milan;
 
 /**
  * @type {Color}
  */
-const paris = ['#EBE6FF', '#9B83F1', '#652CDB'];
+const amsterda = [
+  '#D4FADC',
+  '#AAFDB6',
+  '#83FC8B',
+  '#39924D',
+  '#4ECF6B',
+  '#44B55E',
+  '#3A9A50',
+  '#317D42',
+];
+[
+  amsterda.i10,
+  amsterda.i20,
+  amsterda.i30,
+  amsterda.i40,
+  amsterda.i50,
+  amsterda.i60,
+  amsterda.i70,
+  amsterda.i80,
+] = amsterda;
 
 /**
  * @type {Color}
  */
-const buenosAires = ['#CAE5F5', '#4C9FE4', '#3847C6'];
+const saopaulo = [
+  '#D8F5F5',
+  '#B4F1EA',
+  '#80EEDB',
+  '#6FDCCB',
+  '#58C2B4',
+  '#178173',
+  '#2B9486',
+  '#117D6F',
+];
+[
+  saopaulo.i10,
+  saopaulo.i20,
+  saopaulo.i30,
+  saopaulo.i40,
+  saopaulo.i50,
+  saopaulo.i60,
+  saopaulo.i70,
+  saopaulo.i80,
+] = saopaulo;
+
+/**
+ * @type {Color}
+ */
+const buenosaires = [
+  '#CAE5F5',
+  '#95D4EF',
+  '#6EBCEA',
+  '#4C9FE4',
+  '#3B83DB',
+  '#1968C9',
+  '#0553B1',
+  '#004294',
+];
+[
+  buenosaires.i10,
+  buenosaires.i20,
+  buenosaires.i30,
+  buenosaires.i40,
+  buenosaires.i50,
+  buenosaires.i60,
+  buenosaires.i70,
+  buenosaires.i80,
+] = buenosaires;
+
+/**
+ * @type {Color}
+ */
+const newyork = [
+  '#EBECF9',
+  '#D7DAF3',
+  '#AFB5E8',
+  '#5F6BD2',
+  '#3847C7',
+  '#27318B',
+  '#181E56',
+  '#001027',
+];
+[
+  newyork.i10,
+  newyork.i20,
+  newyork.i30,
+  newyork.i40,
+  newyork.i50,
+  newyork.i60,
+  newyork.i70,
+  newyork.i80,
+] = newyork;
+
+/**
+ * @type {Color}
+ */
+const paris = [
+  '#EBE6FF',
+  '#D8D0FC',
+  '#C5BAF4',
+  '#B3A1F2',
+  '#9B83F1',
+  '#876FDF',
+  '#715EB8',
+  '#5A4A91',
+];
+[
+  paris.i10,
+  paris.i20,
+  paris.i30,
+  paris.i40,
+  paris.i50,
+  paris.i60,
+  paris.i70,
+  paris.i80,
+] = paris;
+/**
+ * @type {Color}
+ */
+const vilaolimpia = [
+  '#F5F5FA',
+  '#E6E6F0',
+  '#D3D3E2',
+  '#C5C5D6',
+  '#A9A9B8',
+  '#9898A7',
+  '#6B6B78',
+  '#41414A',
+];
+[
+  vilaolimpia.i10,
+  vilaolimpia.i20,
+  vilaolimpia.i30,
+  vilaolimpia.i40,
+  vilaolimpia.i50,
+  vilaolimpia.i60,
+  vilaolimpia.i70,
+  vilaolimpia.i80,
+] = vilaolimpia;
 
 const foundation = {
-  text: [],
-  elements: [],
+  text: { primary: '', secondary: '', disabled: '', light: '' },
+  elements: { dropshadow: '', border: '', background: [] },
 };
 
 /**
- * @type {Color}
- */
-const gray = [
-  white, // 0
-  '#F5F5FA', // 1
-  '#E6E6F0', // 2
-  '#DBDBE8', // 3
-  '#D3D3E2', // 4
-  '#C5C5D6', // 5
-  '#A9A9B8', // 6
-  '#9898A6', // 7
-  '#6B6B78', // 8
-  dark, // 9
-];
-
-/**
- * @type {Color}
+ * @type {String}
  */
 const white = '#FFFFFF';
 
 /**
- * @type {Color}
+ * @type {FeedbackColor}
  */
-const dark = '#41414A';
+const positive = [amsterda.i10, amsterda.i70];
 
 /**
- * @type {Color}
+ * @type {FeedbackColor}
  */
-const positive = ['#DEF8E9', '#1E854A'];
+const negative = [madrid.i10, madrid.i70];
 
 /**
- * @type {Color}
+ * @type {FeedbackColor}
  */
-const negative = ['#FCEAE9', '#CB3530'];
+const informative = [buenosaires.i10, buenosaires.i70];
 
 /**
- * @type {Color}
+ * @type {FeedbackColor}
  */
-const informative = ['#E9F3FC', '#2186D9'];
-
-/**
- * @type {Color}
- */
-const warning = ['#FBFBBE', '#CB9303'];
+const warning = [milan.i10, milan.i70];
 
 const colors = {
   madrid,
-  saoPaulo,
-  newYork,
+  munich,
+  milan,
+  amsterda,
+  saopaulo,
+  buenosaires,
+  newyork,
   paris,
-  buenosAires,
+  vilaolimpia,
+  foundation,
+  white,
   positive,
   negative,
   informative,
   warning,
-  gray,
-  white,
-  dark,
 };
 
 export default colors;
